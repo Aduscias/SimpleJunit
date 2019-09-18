8e9a9b2c28b22f0428d95cc42e72870eae59bb43
Ivan Melekhov <ivan.melexov@gmail.com>
2019-09-18 05:58:01 +0300

    initial commit

---
+++ src/Utils.java
@@ -0,0 +1,15 @@
+public class Utils {
+
+
+    public static void main(String[] args){
+        Utils utils = new Utils();
+    }
+
+    private int plus(int number1,int number2){
+        return number1+number2;
+    }
+
+    private float division(float number1,float number2){
+        return number1/number2;
+    }
+}
